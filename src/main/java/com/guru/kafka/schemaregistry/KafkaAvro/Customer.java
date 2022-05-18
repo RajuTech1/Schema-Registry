c1a9462d63eca36fb945a6bf28416c5b37cd9272
RajuTech1 <105433717+RajuTech1@users.noreply.github.com>
2022-05-18 13:28:24 +0530

    Schema Registry v1.0

---
+++ src/main/java/com/guru/kafka/schemaregistry/KafkaAvro/Customer.java
@@ -0,0 +1,57 @@
+package com.guru.kafka.schemaregistry.KafkaAvro;
+
+public class Customer {
+
+	String first_name;
+	String last_name;
+	String age;
+	String height;
+	String weight;
+	String phone_number;
+	String email;
+	
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getHeight() {
+		return height;
+	}
+	public void setHeight(String height) {
+		this.height = height;
+	}
+	public String getWeight() {
+		return weight;
+	}
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+	public String getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
---
+++ src/test/java/com/guru/kafka/schemaregistry/KafkaAvro/KafkaAvroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru.kafka.schemaregistry.KafkaAvro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAvroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
